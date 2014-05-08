a3a2b78e43968b653292bc1fd770fd8985022934
Alex Kersten <alex@dividebyxero.com>
2014-05-08 00:28:48 -0500

    promote version

--- src/io/kersten/thefuzz/Main.java
+++ src/io/kersten/thefuzz/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final String VERSION = "0.9.4";
+    public static final String VERSION = "0.9.5";
 
     public static void main(String[] args) {
         System.out.println("Welcome to TheFuzz.");
